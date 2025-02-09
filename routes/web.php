75622cab1bb049f43fcdb09a5ab87985992b52cd
Nate Beaty <nate@clixel.com>
2025-02-08 23:03:35 -0500

    add routes for sitemap, feed, post type archive page

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 // Route::statamic('example', 'example-view', [
 //    'title' => 'Example'
 // ]);
+
+Route::statamic('posts/type/{type}', 'type')->name('posts.type');
+Route::statamic('sitemap.xml', 'sitemap', ['content_type' => 'text/xml'])->name('sitemap');
+Route::statamic('feed.xml', 'feed', ['content_type' => 'text/xml'])->name('feed');
